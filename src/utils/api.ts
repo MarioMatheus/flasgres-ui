95970c5de7371ecadc299bb0bcd981299085af2f
MarioMatheus <mariomatheu@gmail.com>
2021-03-21 20:56:25 -0300

    add production en

--- src/utils/api.ts
+++ src/utils/api.ts
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 export default axios.create({
-  baseURL: process.env.API_URL || `http://127.0.0.1:5000`,
+  baseURL: process.env.VUE_APP_ROOT_API || `http://127.0.0.1:5000`,
   withCredentials: true,
   headers: {
     Accept: "application/json",
